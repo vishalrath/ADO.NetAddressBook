51a65303791dffc5c411b6246b219d11e3881fea
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-01 23:40:37 +0530

    [vishal] ADO.Net Edit record Sql database connection

---
+++ Uc3ADO.Net_EditContact_AddressBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uc3ADO.Net_EditContact_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AddressBookRepository repo = new AddressBookRepository();
+            repo.UpdateQueryBasedonName();
+        }
+    }
+}
