6efe4b1ee11e95dafff9d2f4247b7042ddbe82bc
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-01 23:46:53 +0530

    [vishal] ADO.Net SortedAlphsbeticaly  Sql database connection

---
+++ Uc8ADO_AddressBook_SortedAlphabetically/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Uc8ADO_AddressBook_SortedAlphabetically
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookRepository repo = new AddressBookRepository();
+            repo.PrintSortDataBasedOnCity();
+        }
+    }
+}
