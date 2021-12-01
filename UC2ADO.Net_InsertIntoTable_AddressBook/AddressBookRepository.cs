5e7f18924e5f407c848f0cdebea27f8e2082068a
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-01 23:39:04 +0530

    [vishal] ADO.Net insert intorecord Sql database connection

---
+++ UC2ADO.Net_InsertIntoTable_AddressBook/AddressBookRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2ADO.Net_InsertIntoTable_AddressBook
+{
+    class AddressBookRepository
+    {
+        //Give path for Database Connection
+        public static string connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Address_Book;TrustServerCertificate=False;";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connection);
+
+        public int InsertIntoTable(Contact addressBook) //parameterize method AddressBook is para
+        {
+            int result = 0;
+            try
+            {
+                using (sqlConnection)
+                {
+                    SqlCommand sqlCommand = new SqlCommand("spInsertIntoTableAddresBook", this.sqlConnection);
+                    //setting command type as stored procedure
+                    sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@id", addressBook.ID_Aook);
+                    sqlCommand.Parameters.AddWithValue("@FirstName", addressBook.FirstName);
+                    sqlCommand.Parameters.AddWithValue("@LastName", addressBook.LastName);
+                    sqlCommand.Parameters.AddWithValue("@Address", addressBook.Address);
+                    sqlCommand.Parameters.AddWithValue("@City", addressBook.City);
+                    sqlCommand.Parameters.AddWithValue("@State", addressBook.State);
+                    sqlCommand.Parameters.AddWithValue("@zip", addressBook.zip);
+                    sqlCommand.Parameters.AddWithValue("@PhoneNumber", addressBook.PhoneNumber);
+                    sqlCommand.Parameters.AddWithValue("@Email", addressBook.Email);
+                    sqlCommand.Parameters.AddWithValue("@addressBookName", addressBook.AddressBookName);
+                    sqlCommand.Parameters.AddWithValue("@addressBookType", addressBook.Type);
+                    sqlConnection.Open();
+                    //Return the number of rows updated
+                    result = sqlCommand.ExecuteNonQuery();
+                    if (result != 0)
+                    {
+                        Console.WriteLine("Inserted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Inserted");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+            return result;
+        }
+    }
+}
---
+++ UC2ADO.Net_InsertIntoTable_AddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UC2ADO.Net_InsertIntoTable_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
