03901de33301fcb97cbc7a531aa6ee90d10bebc6
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-01 23:45:16 +0530

    [vishal] ADO.Net Ability to Count City And State  Sql database connection

---
+++ Uc7ADO_Addressbook_AbilityToCountCityState/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uc7ADO_Addressbook_AbilityToCountCityState
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookRepository repo = new AddressBookRepository();
+            repo.PrintCountDataBasedOnCity();
+            Console.ReadLine();
+        }
+    }
+}
