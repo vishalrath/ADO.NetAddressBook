1c191fdbf197d35730435437b7497b52131f3be8
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-01 23:36:14 +0530

    [vishal] ADO.Net Create Database And Connnected to Sql connection

---
+++ Uc1ADO.Net_DatabsaseConnection_AddressBook/AddressBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc1ADO.Net_DatabsaseConnection_AddressBook
+{
+    class AddressBookRepository
+    {
+        //Give path for Database Connection
+        public static string connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Address_Book;TrustServerCertificate=False;";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connection);
+    }
+}
---
+++ Uc1ADO.Net_DatabsaseConnection_AddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uc1ADO.Net_DatabsaseConnection_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
